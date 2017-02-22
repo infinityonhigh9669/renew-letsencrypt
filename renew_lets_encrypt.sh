a70c43120a6bee73fed74ee1707e0e5510daa120
T-Bone Lu <infinityonhigh9669@gmail.com>
2017-02-22 10:28:05 +0800

    First commit by T-Bone Lu.

---
+++ renew_lets_encrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author: T-Bone Lu
+
+PATH_DIRECTORY_LETS_ENCRYPT=/opt/letsencrypt/ #Your letsencrypt folder
+PATH_FILE_LOG_SCRIPT=/var/log/le-renew.log
+NOW=$(date +"%Y-%m-%d %T")
+
+echo "***************************************************************************" >> $PATH_FILE_LOG_SCRIPT
+echo "Start Script - $NOW" >> $PATH_FILE_LOG_SCRIPT
+cd $PATH_DIRECTORY_LETS_ENCRYPT
+git pull origin master >>  $PATH_FILE_LOG_SCRIPT 2>&1
+$PATH_DIRECTORY_LETS_ENCRYPT/letsencrypt-auto renew >> $PATH_FILE_LOG_SCRIPT
+echo "End Script - $NOW" >> $PATH_FILE_LOG_SCRIPT
